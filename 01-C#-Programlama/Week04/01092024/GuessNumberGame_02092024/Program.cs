ef21ace4757e9626cb4ab233daa5262a90f4d43a
Dogukan Babayigit <doukanb@proton.me>
2024-09-02 11:35:01 +0300

    02092024-GuessNumberGame-Tekrar
    02092024-GuessNumberGame-Tekrar

---
+++ 01-C#-Programlama/Week04/01092024/GuessNumberGame_02092024/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace GuessNumberGame_02092024;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int tahmin;
+        string cevap = "";
+        Random rnd = new();
+        int sayi = rnd.Next(1, 101);
+        int tahminHakki = 5;
+
+        System.Console.WriteLine("Sayı tahmin oyununa hoş geldiniz!");
+        System.Console.WriteLine("1 ile 100 arasında bir sayı tuttum.Tahmin etmeye çalışın.");
+
+        do
+        {
+            Console.Write("Tahmininiz: ");
+            tahmin = Convert.ToInt32(Console.ReadLine());
+
+            tahminHakki--;
+            if (tahmin == sayi)
+            {
+                System.Console.WriteLine("Tebrikler , Kazandınız!");
+            }
+            else if (tahmin < sayi)
+            {
+                System.Console.WriteLine("Daha Büyük Bir Sayi >Giriniz.");
+            }
+            else
+            {
+                System.Console.WriteLine("Daha Küçük Bir Sayi Giriniz. ");
+            }
+        } while (tahminHakki > 0 && sayi != tahmin);
+
+        if (tahminHakki == 0)
+        {
+            Console.WriteLine($"Tahmin haklarınız bitti. Tuttuğum sayı: {sayi}");
+        }
+
+        System.Console.WriteLine("Tekrar Oynamak İster Misin? (evet/hayır)");
+
+        if (cevap == "evet")
+        {
+            Main(args);
+        }
+
+        else
+        {
+            Console.WriteLine("Oyun sona erdi.");
+        }
+
+    }
+}
---
+++ 01-C#-Programlama/Week04/01092024/Project13_GuessNumberGame/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Project13_GuessNumberGame;
+
+/* Sayı tahmin oyunu = uygulamanın rastgele üreteceği 1-100 arasındaki bir sayıyı kullanıcın tahmin ederek
+bulmasını sağlayan bir kod yazınız. 
+*Kullanın tahmini üretilen sayıdan küçükse ya da byükse buna göre yönlendirme yapılsın
+*Kullanıcın 5 Ttahmin hakkı olsun.
+*Kullanıcı doğru tahmin yaptıysa ya da tahmin hakkı dolduysa uygun mesajı vererek oyun sona ersin.
+*/
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string answer;
+        Random rnd = new Random();
+        int generatedNumber = rnd.Next(1, 101);
+        System.Console.WriteLine($"HİLE=> {generatedNumber}");
+        System.Console.WriteLine("*****************");
+        System.Console.WriteLine();
+
+        int guessNo;
+        int live = 1;
+        int liveLimit = 5;
+        string resultMessage = "";
+
+        do
+        {
+
+            Console.Write($"{live}.Hak(1-100): ");
+            guessNo = int.Parse(Console.ReadLine());
+
+            if (guessNo < generatedNumber)
+            {
+                resultMessage = "Daha büyük bir sayı dene.";
+            }
+
+            else if (guessNo > generatedNumber)
+            {
+                resultMessage = "Daha küçük bir sayı dene.";
+            }
+
+            if (guessNo != generatedNumber)
+            {
+                live++;
+            }
+
+            if (live <= liveLimit && guessNo != generatedNumber)
+            {
+                System.Console.WriteLine(resultMessage);
+            }
+
+            System.Console.WriteLine("Tekrar oynamak ister misin ? (E/H)");
+            System.Console.WriteLine("-----------------------------------");
+            answer = Console.ReadLine();
+            if (answer != "E")
+            {
+                Console.WriteLine("\nSayı Tahmin Oyununu oynadığınız için teşekkürler!");
+            }
+
+        } while (guessNo != generatedNumber && live <= liveLimit && answer != "H");
+        resultMessage = guessNo == generatedNumber ? "Kazandın" : "Kaybettin";
+        System.Console.WriteLine(resultMessage);
+    }
+}
