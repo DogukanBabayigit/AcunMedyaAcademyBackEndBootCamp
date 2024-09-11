f0ab231d9a38e6d56dc9a08bc8638f9db676aef0
Dogukan Babayigit <doukanb@proton.me>
2024-09-11 09:36:37 +0300

    Methods
    07*09*2024

---
+++ 01-C#-Programlama/Week05/08-08-2024/Project17_MethodOverloading/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+using System.Xml;
+
+namespace Project17_MethodOverloading;
+
+class Program
+{
+    // Aymı method gerektiğinde kullanılabilir fakat veri eklenmediği veya değişmediğinde hata verecektir.
+
+    // static int Sum(int a, int b)
+    // {
+    //     return a + b;
+    // }
+    // static int Sum(int a, int b, int c)
+    // {
+    //     return a + b + c;
+    // }
+    // static int Sum(int a, int b, int c, int d)
+    // {
+    //     return a + b + c + d;
+    // }
+
+    // static void Greet(string name, byte age)
+    // {
+    //     System.Console.WriteLine($"Merhaba {age} yaşındaki {name}");
+    // }
+
+    /////////////////////////////////////////////////
+
+    static string GetCoffee(string coffeeType, int quantitySugar = 0, bool withMilk = true)
+    {
+        string coffee = $"{coffeeType} kahve";
+        coffee += quantitySugar > 0 ? $"{quantitySugar} sekerli," : " sekersiz,";
+        if (withMilk)
+        {
+            coffee += " Sütlü";
+        }
+        else
+            coffee += " Sütsüz";
+        coffee += "\nKahveniz hazir. Afiyet Olsun:D";
+        return coffee;
+    }
+    static void Main(string[] args)
+    {
+
+        System.Console.WriteLine(GetCoffee("Americano "));
+        System.Console.WriteLine(GetCoffee("Americano ", 2));
+        System.Console.WriteLine(GetCoffee("Americano ", 5, false));
+
+
+
+
+
+
+
+        // Console.WriteLine(Sum(5, 7));
+        // Console.WriteLine(Sum(5, 7, 8));
+        // Console.WriteLine(Sum(5, 7, 8, 9));
+        // Greet("Engin", 49);
+
+
+    }
+}
---
+++ 01-C#-Programlama/Week05/08-08-2024/Project18_MethodSamples/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using System.Numerics;
+
+namespace Project18_MethodSamples;
+
+class Program
+{
+    //Kendisine gönderilen int tipindeli sayi dizininde yer alan negatif sayilari geri döndüren bir metot hazirlayalim.
+
+    static List<int> GetNegativeNumbers(List<int> numbers)
+    {
+        List<int> negativeNumbers = new List<int>();
+        foreach (var number in numbers)
+        {
+            if (number < 0)
+            {
+                negativeNumbers.Add(number);
+            }
+
+        }
+        return negativeNumbers;
+    }
+
+    static int TotalOfSquares(int sayi1, int sayi2)
+    {
+        // int square1 = sayi1 * sayi1;
+        // int square2 = sayi2 * sayi2;
+        // int result = square1 + square2;
+        // return result;
+        return (sayi1 * sayi1) + (sayi2 * sayi2);
+    }
+
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine(TotalOfSquares(4, 7));
+        System.Console.WriteLine(TotalOfSquares(5, 7));
+        System.Console.WriteLine(TotalOfSquares(2, 8));
+
+
+
+
+        // List<int> numbers = [5, 7, 8, -5, -4, 3, -12, 4, 5, 7];
+        // List<int> resultNumbers = GetNegativeNumbers(numbers);
+        // foreach (var number in resultNumbers)
+        // {
+        //     System.Console.WriteLine(number);
+        // }
+    }
+}
