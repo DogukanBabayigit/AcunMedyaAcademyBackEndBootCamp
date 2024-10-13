70aaa5c262d9a009c483474143115111c8405d54
Dogukan Babayigit <doukanb@proton.me>
2024-10-13 12:18:07 +0300

    12-10-2024 HTML-CSS-JS

---
+++ 13-10-2024 Hocadan Çekilen Repo/04-Web/Week09/06-10-2024/04-JavaScript/script.js
@@ -0,0 +1,7 @@
+document.getElementById('button-external').addEventListener('mouseover', function () {
+    alert('İşte üzerime geldin!');
+});
+
+document.getElementById('button-external').addEventListener('dblclick', function () {
+    alert('İşte çift tıkladın!!!')
+});
